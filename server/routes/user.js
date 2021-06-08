39567a83fd71556307fc4f27acfa52337801b8cd
Carmine Choi <carmine.choi@gmail.com>
2021-06-08 16:23:20 -0700

    Signup route made with no duplicate prevention

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user-models');
+
+router.post('/signup', async (req, res) => {
+    let newUser = new User();
+
+    newUser.username = req.body.username;
+
+    newUser.email = req.body.email;
+    newUser.password = req.body.password;
+
+    newUser.save((err, User) => {
+        if (err) {
+            return res.status(400).json({
+                message: "Failed to add user",
+            });
+        } else {
+            return res.status(201).json({
+                message: "User added sucessfully",
+            });
+        }
+    });
+}); 
 
 router.get('/login', async (req, res) => {
     try {
