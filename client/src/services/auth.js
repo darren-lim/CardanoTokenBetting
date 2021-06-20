8d48c2c562c273555eb2c8bc193edc8f35bfb359
Carmine <carmine.choi@gmail.com>
2021-06-20 04:21:03 -0700

    Added client authentication service

---
+++ client/src/services/auth.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import acios from 'axios';
+import { response } from 'express';
+
+const API_URL = "http://localhost:5000/api/"
+
+class AuthService {
+    // POST new user info
+    register(username, email, password) {
+        return axios.post(API_URL + 'signup', { username, email, password });
+    }
+
+    // POST login info and receive/store jwt token
+    login(username, password) {
+        return axios
+            .post(API_URL + 'login', { username, password })
+            .then((response) => {
+                if (response.data.accessToken) {
+                    localStorage.setItem('user', JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
+
+    // Delete jwt token
+    logout() {
+        localStorage.removeItem('user');
+    }
+}
+
+export default new AuthService();
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user-models');
 
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res) => {
-    
     let newUser = new User();
     newUser.username = req.body.username;
     newUser.email = req.body.email;
@@ -33,17 +33,22 @@ router.post('/login', async (req, res) => {
             });
         // If user exists, check if password is correct
         } else {
-            if (await user.passwordIsValid(req.body.password)) {
-                return res.status(400).json({
-                    message: "User logged in",
-                });
-            } else {
+            if (!await user.passwordIsValid(req.body.password)) {
                 return res.status(400).json({
                     message: "Incorrect password",
                 });
             }
+
+            var token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET, {expiresIn: 86400});
+
+            res.status(200).send({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                accessToken: token,
+            })
         }
     });
 }); 
 
-module.exports = router; 
+module.exports = router;  
