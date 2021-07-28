76ffba60651c2750ebd736b3439fb95fbe1a4673
Carmine Choi <carmine.choi@gmail.com>
2021-07-27 17:29:58 -0700

    SYNC: card flips in progress

--- client/src/components/Board/Card-component.js
+++ client/src/components/Board/Card-component.js
@@ -1,26 +1,43 @@
-import React, {  } from "react";
+import React, { useState } from "react";
 
 import burger from '../../assets/burger-card-logo.png';
+import burnt from '../../assets/patty.png'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './Card-component.css'
 
-function Card({ isFaceDown }) {
+function Card() {
+    const [flipped, setFlipped] = useState(false);
+
+
     return (
-        <div className="play-card position-relative m-1 p-3">
-            { isFaceDown ?
-                <div className="play-card-face-down text-center">
-                    <div className="card-image-containter">
-                        <img src={burger} height="60" alt="burger" />
-                    </div>
-                </div>
+        <div className="play-card position-relative m-1 p-3"
+            onClick={setFlipped(true)}>    
+            { flipped ?
+                <CardFaceDown />
             :
-                <div className="play-card-face-up text-center">
-                    <div className="card-image-containter">
-                        <img src={burger} alt="burger" />
-                    </div>
-                </div>
+                <CardFaceUp />
             }
+        </div>
+    )
+}
+
+function CardFaceDown() {
+    return (
+        <div className="play-card-face-down text-center">
+            <div className="card-image-container">
+                <img src={burger} height="60" alt="burger" />
+            </div>
+        </div>
+    )
+}
+
+function CardFaceUp() {
+    return (
+        <div className="play-card-face-up text-center">
+            <div className="card-image-container">
+                <img src={burger} height="60" alt="burger" />
+            </div>
         </div>
     )
 }
