3585d2b15aec42ad8e4b0b1553f37718226a92fb
Carmine Choi <carmine.choi@gmail.com>
2021-07-30 16:30:29 -0700

    SYNC

--- client/src/components/Board/Bet-component.js
+++ client/src/components/Board/Bet-component.js
@@ -5,26 +5,26 @@ import logo from '../../assets/burgerLogo.png';
 import 'bootstrap/dist/css/bootstrap.css'; 
 
 function Bet(props) {
+
+    const [balance, setBalance] = useState(1000);
+
     function handleDeposit() {
         // redirect to deposit page
 
         return null;
     }
 
-    function getBalance() {
-        // getBalance from services
-        return "10000";
-    }
-
     return (
         <div>
             <div className="balance-container">
                 <form onSubmit={handleDeposit}>
-                    <label className="balance-label">Balance</label>
                     <div className="balance-box-container">
-                        <img src={logo} height="20" alt="balance-coin" ></img>
-                        <p className="balance-amount">{getBalance}</p>
-                        <input className="btn active" type="submit" value="Deposit"/>
+                        <label className="balance-label">Balance</label>
+                        <div className="balance-display">
+                            <img src={logo} height="20" alt="balance-coin" ></img>
+                            <p className="balance-amount">{balance}</p>
+                            <input className="btn active" type="submit" value="Deposit"/>
+                        </div>
                     </div>
                 </form>
             </div>
